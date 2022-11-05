c0d9a7008cf4b19d57a207e6441e40678aead918
ali-narimanov <ali.narimanli@mail.ru>
2022-11-05 19:21:03 +0400

    #9thCommit

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,9 @@ let ul = document.querySelector('ul');
 let poisk = document.querySelector('.poisk');
 let iks = document.querySelector('.iks');
 let input = document.querySelector('input');
-
+let but = document.querySelector('.but')
+let photo1 = document.querySelector('.photo1');
+let photo2 = document.querySelector('.photo2')
 
 
 
@@ -42,6 +44,11 @@ ul.addEventListener("click",(e)=>{
 })
 
 
+
+
+
+
+
 add.addEventListener('click', ()=>{
     console.log(" 2nd works")
     let li = document.createElement('li');
@@ -59,6 +66,10 @@ add.addEventListener('click', ()=>{
   delButton.classList.add('delButton')
 })
 
+
+
+
+
 let a = document.querySelector('.iks')
 a.addEventListener('click',()=>{
   poisk.classList.add('hide')
@@ -71,6 +82,47 @@ plus.addEventListener('click',()=>{
     poisk.classList.remove("hide")
   }
 })
+
+
+
+
+photo2.style.display="none"
+photo1.addEventListener('click', ()=>{
+  photo1.style.display="none";
+  photo2.style.display="block"
+  
+  let arr = [];
+  let info = [...ul.children];
+  info.forEach(item=>{
+    arr.push(item.innerHTML);
+  })
+  arr.sort();
+  ul.innerHTML="";
+  arr.forEach(item=>{
+    let lis = `<li>${item}</li>`;
+    ul.innerHTML +=lis;
+  });
+});
+photo2.addEventListener('click', ()=>{
+  photo2.style.display="none";
+  photo1.style.display="block"
+  
+  let arr = [];
+  let info = [...ul.children];
+  info.forEach(item=>{
+    arr.push(item.innerHTML);
+  })
+  arr.sort();
+  arr.reverse();
+  ul.innerHTML="";
+  arr.forEach(item=>{
+    let lis = `<li>${item}</li>`;
+    ul.innerHTML +=lis;
+  });
+});
+
+
+
 
 
 
